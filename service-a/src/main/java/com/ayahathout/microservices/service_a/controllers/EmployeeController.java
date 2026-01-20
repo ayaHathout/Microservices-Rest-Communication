6f683fc9b9c8f2117b76452a6beaea047fe3bd53
Aya Hathout <Aya.hathout@edafa.com>
2026-01-20 13:05:12 +0200

    Add controller into service-a

---
+++ service-a/src/main/java/com/ayahathout/microservices/service_a/controllers/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.ayahathout.microservices.service_a.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/employees")
+public class EmployeeController {
+
+    @GetMapping
+    public String getEmployeeInfo() {
+        return "Aya Hathout";
+    }
+}
