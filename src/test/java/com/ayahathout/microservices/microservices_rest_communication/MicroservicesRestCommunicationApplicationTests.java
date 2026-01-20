96400b4ca1326426e5e74a229effd41178c2a0cc
Aya Hathout <Aya.hathout@edafa.com>
2026-01-20 11:15:54 +0200

    Initialize project structure

---
+++ src/test/java/com/ayahathout/microservices/microservices_rest_communication/MicroservicesRestCommunicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayahathout.microservices.microservices_rest_communication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesRestCommunicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
