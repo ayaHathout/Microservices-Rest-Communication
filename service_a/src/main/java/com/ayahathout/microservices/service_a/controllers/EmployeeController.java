3a5847578bd04c0a0aa61a21e9a757f95acc8b83
Aya <ayahathout1@gmail.com>
2026-01-30 17:54:38 +0200

    use rest template to communicate between service-a & service-b

--- service_a/src/main/java/com/ayahathout/microservices/service_a/controllers/EmployeeController.java
+++ service_a/src/main/java/com/ayahathout/microservices/service_a/controllers/EmployeeController.java
@@ -1,14 +1,22 @@
 package com.ayahathout.microservices.service_a.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping
     public String getEmployeeInfo() {
-        return "Aya Hathout";
+        String address = restTemplate.getForObject("http://localhost:8082/addresses", String.class);
+
+        return "Aya Hathout, " + address;
     }
 }
